ebd017b0e9753d5c1dd3cbda9fa20af80f670c41
Chen <developerjoy@gmail.com>
2015-08-05 11:25:29 +0800

    svn 程式碼更新測試。

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,11 +5,11 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	  // just for test...
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// just for test.
 	}
 }
