ae5f15321d145332ad0271c03a83fb13ffadc856
Chen <developerjoy@gmail.com>
2015-08-10 16:56:17 +0800

    加上忽略檔案、目錄。

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class TestScript : MonoBehaviour
+{
+
+    private Vector3 val;
 
 	// Use this for initialization
 	void Start () {
